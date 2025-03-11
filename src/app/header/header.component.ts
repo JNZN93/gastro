8b26f6cb581b697352d8a98053d9430ca2559ac0
FiratTasyurdu <131069064+JNZN93@users.noreply.github.com>
2025-03-11 20:25:16 +0100

    implement logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToggleCartService } from '../toggle-cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,31 @@ import { ToggleCartService } from '../toggle-cart.service';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  showModal = false;
+  private hiddenRoutes: string[] = ['/login', '/registration'];
 
-  constructor(private toggleService: ToggleCartService) {}
+  constructor(private toggleService: ToggleCartService, private router: Router) {}
 
   toggleWarenkorb() {
     this.toggleService.toggle(); // Toggle-Funktion aufrufen
   }
 
+  openModal() {
+    this.showModal = true;
+
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    this.showModal = false;
+    location.reload();
+  }
+
+  goBack() {
+    this.showModal = false;
+  }
+
+  shouldHideElement(): boolean {
+    return this.hiddenRoutes.includes(this.router.url);
+  }
 }
