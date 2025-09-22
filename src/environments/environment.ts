da79e85c3bf066af672ecd0cb353ca03fe542be6
philyas <lephi@gmx.net>
2025-09-22 21:35:22 +0200

    add prod and development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://multi-mandant-ecommerce.onrender.com'
+  apiUrl: 'http://localhost:10000'
+  //apiUrl: 'https://multi-mandant-ecommerce.onrender.com'
 };
