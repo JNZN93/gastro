fd3d9cf275db911766a4e097feb485c627a6a235
FiratTasyurdu <131069064+JNZN93@users.noreply.github.com>
2025-10-19 23:49:48 +0200

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:10000'
-  apiUrl: 'https://multi-mandant-ecommerce.onrender.com'
+  apiUrl: 'http://localhost:10000'
+  //apiUrl: 'https://multi-mandant-ecommerce.onrender.com'
 };
